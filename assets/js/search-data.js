b33c184d5ea9c3253c290ee6d59089d74f1c2bb0
anandme01 <anandiitk01@gmail.com>
2025-07-25 18:28:40 +0000

    Deploying to gh-pages from @ anandme01/anandme01.github.io@c8b21ca902888dd6c156e04be5fcc29dfaa13026 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-knowledge-sharing",
           title: "Knowledge Sharing",
           description: "Learning is a lifelong journey, and this is my humble effort to give back to society what I have gained along the way.",
