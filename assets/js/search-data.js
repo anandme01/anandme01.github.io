32e98286b713e178afc8a4d0a0a46af8bf898327
anandme01 <anandiitk01@gmail.com>
2025-07-29 17:50:45 +0000

    Deploying to gh-pages from @ anandme01/anandme01.github.io@35fb162f5c0f8ab06e74f86618d4bbc2b82af19e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,8 +413,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-i-am-excited-to-share-that-our-paper-data-aided-csi-estimation-using-affine-precoded-superimposed-pilots-in-otfs-mimo-systems-has-been-accepted-in-ieee-transactions-on-communications",
+          title: 'I am Excited to share that our paper “Data-Aided CSI Estimation Using Affine-Precoded...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
