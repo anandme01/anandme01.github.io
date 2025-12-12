7d588e27ec82dc87377efe1760e9e89fc8be8ad3
anandme01 <anandiitk01@gmail.com>
2025-12-12 15:45:55 +0000

    Deploying to gh-pages from @ anandme01/anandme01.github.io@f84eced5463a4406b59684dff239e756b2c06462 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,10 +408,12 @@ ninja.data = [{
           section: "News",},{id: "news-our-work-is-accepted-in-ieee-globecom-workshop-secure-hybrid-precoding-in-mmwave-mimo-otfs-system-in-the-presence-of-transmitter-iqi",
           title: 'Our work is accepted in IEEE Globecom Workshop: “Secure Hybrid Precoding in mmWave...',
           description: "",
-          section: "News",},{id: "news-i-am-glad-to-inform-you-that-i-am-the-runner-up-in-4-minute-thesis-4mt-competition-organized-at-ieee-globecom",
-          title: 'I am glad to inform you that I am the runner-up in 4...',
+          section: "News",},{id: "news-announcement-9",
+          title: 'Announcement_9',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_9/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
